f39f12a13ba3cb74767000b2f8212059db803a1e
Léo Lhomme <leolhommeo@gmail.com>
2023-03-28 14:57:57 +0200

    creation du repo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.example.ex02.Exo2;
+import org.example.exo1.Exo1;
+import org.example.exo3.Exo3;
+
+public class Main {
+    public static void main(String[] args) {
+        //Exo1.getCarre();
+        //Exo2.getExo2();
+        //Exo3.getPrixTTC();
+    }
+}
---
+++ src/main/java/org/example/ex02/Exo2.java
@@ -0,0 +1,13 @@
+package org.example.ex02;
+
+import java.util.Scanner;
+
+public class Exo2 {
+    public static void getExo2(){
+        String name;
+        System.out.println("Quel est votre nom :");
+        Scanner sc = new Scanner(System.in);
+        name = sc.next();
+        System.out.println("Bonjour "+name);
+    }
+}
