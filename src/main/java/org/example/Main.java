8e9d90a00df6e49bc81d5329911c6a2a43b82220
Léo Lhomme <leolhommeo@gmail.com>
2023-04-13 11:13:14 +0200

    ajout des stream sur l'exo gestionBook

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import org.example.ManagementBook.MainBook;
 import org.example.Stream.Exo1.MainStream;
 
 public class Main {
     public static void main(String[] args) {
-        MainStream.main();
+        //MainStream.main();
+        MainBook.main();
     }
 }
--- src/main/java/org/example/ManagementBook/ClasseServices/BookServices.java
+++ src/main/java/org/example/ManagementBook/ClasseServices/BookServices.java
@@ -1,54 +1,73 @@
 package org.example.ManagementBook.ClasseServices;
 
-import com.sun.source.tree.LambdaExpressionTree;
 import org.example.ManagementBook.Classes.Autor;
 import org.example.ManagementBook.Classes.Book;
 import org.example.ManagementBook.Classes.Publisher;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookServices {
     public List<Book> filterBooksByAuthor(Autor auteur, Book[] livres) {
-        List<Book> livreTrie = new ArrayList<Book>();
-        for (Book livre:livres) {
-            Autor[] tabs = livre.getAuteurs();
-            for (Autor autor :tabs) {
-                if(autor.equals(auteur)){
-                    livreTrie.add(livre);
-                }
+        List<Book> livreTrie = Arrays.stream(livres).filter(l ->{
+            List<Autor> auth = Arrays.asList(l.getAuteurs());
+            if(auth.contains(auteur)){
+                return true;
             }
-        }
+            else{
+                return false;
+            }
+        }).collect(Collectors.toList());
+
         return livreTrie;
+
+//        for (Book livre:livres) {
+//            Autor[] tabs = livre.getAuteurs();
+//            for (Autor autor :tabs) {
+//                if(autor.equals(auteur)){
+//                    livreTrie.add(livre);
+//                }
+//            }
+//        }
+
     }
 
     public Book[] filterBooksByPublisher(Publisher editeur, Book[] livres) {
-        Book[] livreTrie ={};
-        for (Book livre:livres) {
-            if (livre.getPublisher().equals(editeur) ){
-                Book[] tmptab = new Book[livreTrie.length+1];
-                for (int i = 0; i < livreTrie.length; i++) {
-                    tmptab[i] = livreTrie[i];
-                }
-                tmptab[livreTrie.length]=livre;
-                livreTrie = tmptab;
-            }
-        }
+        Book[] livreTrie = Arrays.stream(livres)
+                .filter(l -> l.getPublisher().equals(editeur))
+                .toArray(Book[]::new);
+
         return  livreTrie;
+
+//        for (Book livre:livres) {
+//            if (livre.getPublisher().equals(editeur) ){
+//                Book[] tmptab = new Book[livreTrie.length+1];
+//                for (int i = 0; i < livreTrie.length; i++) {
+//                    tmptab[i] = livreTrie[i];
+//                }
+//                tmptab[livreTrie.length]=livre;
+//                livreTrie = tmptab;
+//            }
+//        }
     }
 
     public Book[] filterBooksAfterSpecifiedYear(int yearFromInclusively, Book[] livres) {
-        Book[] livreTrie ={};
-        for (Book livre:livres) {
-            if (livre.getPublishingYear()==yearFromInclusively){
-                Book[] tmptab = new Book[livreTrie.length+1];
-                for (int i = 0; i < livreTrie.length; i++) {
-                    tmptab[i] = livreTrie[i];
-                }
-                tmptab[livreTrie.length]=livre;
-                livreTrie = tmptab;
-            }
-        }
+        Book[] livreTrie = Arrays.stream(livres)
+                .filter(l-> l.getPublishingYear() >= yearFromInclusively)
+                .toArray(Book[]::new);
+
         return  livreTrie;
+
+//        for (Book livre:livres) {
+//            if (livre.getPublishingYear()==yearFromInclusively){
+//                Book[] tmptab = new Book[livreTrie.length+1];
+//                for (int i = 0; i < livreTrie.length; i++) {
+//                    tmptab[i] = livreTrie[i];
+//                }
+//                tmptab[livreTrie.length]=livre;
+//                livreTrie = tmptab;
+//            }
+//        }
     }
 }
